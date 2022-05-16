0a089205c9370bbeea686640036f43afa1334b84
khalidamairi3 <khalidamairi3@gmail.com>
2022-05-16 17:55:16 -0600

    notification-integration

--- src/main/java/com/project2/telemedicineapi/services/AppointmentService.java
+++ src/main/java/com/project2/telemedicineapi/services/AppointmentService.java
@@ -5,6 +5,7 @@ import com.project2.telemedicineapi.dto.AppointmentRequestDTO;
 import com.project2.telemedicineapi.entities.Appointment;
 import com.project2.telemedicineapi.entities.Doctor;
 import com.project2.telemedicineapi.entities.Patient;
+import com.project2.telemedicineapi.helpers.NotificationClient;
 import com.project2.telemedicineapi.repositories.AppointmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -33,6 +34,8 @@ public class AppointmentService {
         appointment.setPatient(patient);
         appointment.setDoctor(doctor);
         appointmentRepository.save(appointment);
+        NotificationClient notificationClient= new NotificationClient();
+        notificationClient.callPostEmail(doctor.getPhoneNum(),"Hey " + doctor.getUsername() + ", you have a new appointment request from "+  patient.getUsername() );
     }
 
     public List<Appointment> getAll() {
@@ -56,6 +59,8 @@ public class AppointmentService {
     public void updateStatus(int id, String status){
         Appointment appointment = getAppointment(id);
         appointment.setStatus(status);
+        NotificationClient notificationClient= new NotificationClient();
+        notificationClient.callPostEmail(appointment.getPatient().getPhoneNum(),"Hey " + appointment.getPatient().getUsername() + ", your appointment on " + appointment.getDateTime() + " has been " +appointment.getStatus() +" by "+  appointment.getDoctor().getUsername() );
         appointmentRepository.save(appointment);
     }
 
